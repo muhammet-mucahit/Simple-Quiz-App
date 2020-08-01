a33ac2c72dccda909e6dc8d5678ada85477ae6d1
muhammet-mucahit <mucahitaktepe@gmail.com>
2020-08-02 02:42:24 +0300

    update readme

--- lib/main.dart
+++ lib/main.dart
@@ -44,6 +44,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
           appBar: AppBar(
             title: Text('Flutter App'),
